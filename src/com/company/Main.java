17ed206bfd9912ff44da5a1e045890ca98a4faa8
natashalysakova <natashalysakova@gmail.com>
2015-09-10 18:57:20 +0300

    new pool

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -27,7 +27,7 @@ public class Main {
 
         d.playSnare();
         d.playTopHat();*/
-
+/*
         Echo e1 = new Echo();
         Echo e2 = e1;
 
@@ -44,9 +44,52 @@ public class Main {
             }
             x++;
         }
-        System.out.println(e2.count);
+        System.out.println(e2.count);*/
+
+/*
+        String[] islands = new String[4];
+        islands[0] = "Бермуды";
+        islands[1] = "Фиджи";
+        islands[2] = "Азорские острова";
+        islands[3] = "Косумель";
+
+        int[] index = new int[4];
+        index[0] = 1;
+        index[1] = 3;
+        index[2] = 0;
+        index[3] = 2;
+
+        int y = 0;
+        int ref;
+
+        while (y < 4) {
+
+            ref = index[y];
+
+            *//*System.out.print("острова = ");*//*
+            System.out.println(islands[ref]);
+            y = y + 1;
+        }*/
+
+        Triangle ta = new Triangle();
+
+        while (x < 4){
+            Triangle[] t = new Triangle[4];
+            ta.height = (x + 1) * 2;
+            ta.length = x + 4;
+
+            System.out.print("треугольник " + x + ", зона");
+            System.out.println(" = " + .area);
+
         }
+
+        x = 27;
+        Triangle t5 = ta[2];
+        ta[2].area = 343;
+        System.out.print("y = " + y);
+        System.out.println(", зона t5 = " + t5.area);
     }
+}
 
 
 
---
+++ src/com/company/Practice/Triangle.java
@@ -0,0 +1,15 @@
+package com.company.Practice;
+
+/**
+ * Created by quantark on 10.09.15.
+ */
+public class Triangle {
+
+    public double area;
+    public int height;
+    public int length;
+
+    public void setArea(){
+       area = height * length / 2;
+    }
+}
